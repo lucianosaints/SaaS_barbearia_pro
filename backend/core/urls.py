d2761965d9f3100da086fc75b25be65bcd189d98
luciano saints <lucinaosaints.adm@gmail.com>
2026-07-09 21:51:23 -0300

    fix(media): resolve erro 404 e mixed content de imagens em producao

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.static import serve
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
@@ -51,3 +53,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Em produção, garante que a rota de media funcione via Gunicorn/Django
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
